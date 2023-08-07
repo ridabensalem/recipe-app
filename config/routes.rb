793141ebff90c15ced6f1e5732c2c28a69c1fc6b
prius2055 <prius2055@gmail.com>
2023-08-07 16:08:07 +0100

    Update config/routes.rb to show associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
-  resources :recipes
-  resources :foods
-  resources :users
+  
+  root "foods#index"
+  
+  resources :users do
+    resources :foods do
+      resources :recipe_foods
+    end
+    resources :recipes do
+      resources :recipe_foods
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
-
-
