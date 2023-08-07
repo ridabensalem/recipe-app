63008f8f6d55374c5bef2e24a35e32e93693d81e
prius2055 <prius2055@gmail.com>
2023-08-07 16:12:21 +0100

    Fix linters errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  
+ 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
   root "foods#index"
   
   resources :users do
@@ -10,8 +15,4 @@ Rails.application.routes.draw do
       resources :recipe_foods
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
