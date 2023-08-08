8c6415ac33c2380ab5af64618e3d489e916b31e4
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-08 11:29:29 +0100

    Add authentication functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
