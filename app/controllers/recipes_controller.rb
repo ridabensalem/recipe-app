1adca166d0350397db6eb61e8a08997398cf98a6
prius2055 <prius2055@gmail.com>
2023-08-12 11:49:15 +0100

    fix bugs with recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: %i[show edit update destroy]
+  # load_and_authorize_resource
 
   # GET /recipes or /recipes.json
   def index
@@ -7,11 +8,12 @@ class RecipesController < ApplicationController
   end
 
   def public_recipes
-    @public = Recipe.where(public: true).includes(:recipe_foods)
+    @public = Recipe.where(public: true).includes(:user, :recipe_foods).order('created_at DESC')
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show; 
+  end
 
   # GET /recipes/new
   def new
